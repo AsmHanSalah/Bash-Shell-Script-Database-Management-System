c60c2b92527c283015053b3a6e3d6cd1fb26ad43
AsmHanSalah <asmhan_salah@outlook.com>
2024-08-27 08:56:03 +0300

    ADD 1. Create New Database option to main menu

--- App.sh
+++ App.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+while true; do
+
+    echo "Main Menu:"
+    echo "1. Create Database"
+    read -p "Choose an option (1): " choice
+
+    case $choice in
+        1) echo "Function not implemented yet." ;;
+        *) echo "Invalid option, please try again." ;;
+    esac
+
+    echo
+
+done
