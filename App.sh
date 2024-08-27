17fbbede08059cdeb210ca7f6bf44f1bb51a8750
AsmHanSalah <asmhan_salah@outlook.com>
2024-08-27 09:15:37 +0300

    ADD connect_to_database function

--- App.sh
+++ App.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Global variable to hold the current database name
+current_db=""
+
 create_database() {
 
     read -p "Enter database name: " db_name
@@ -20,6 +23,21 @@ list_databases() {
 
 }
 
+connect_to_database() {
+
+    read -p "Enter database name to connect: " db_name
+    
+    if [ -d "$db_name" ]; then
+        echo "Connected to database '$db_name' successfully."
+        cd "$db_name"
+        current_db="$db_name"
+        echo "Database menu not implemented yet."
+    else
+        echo "Database '$db_name' does not exist."
+    fi
+
+}
+
 while true; do
 
     echo "Main Menu:"
@@ -32,7 +50,7 @@ while true; do
     case $choice in
         1) create_database ;;
         2) list_databases ;;
-        3) echo "Function not implemented yet." ;;
+        3) connect_to_database ;;
         4) echo "Exiting program..."; exit 0 ;;
         *) echo "Invalid option, please try again." ;;
     esac
